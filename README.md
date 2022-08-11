5e0636ddf965ba9dbd4115f23a485537107496a7
Jane McPheron <jvmcpheron@Janes-MacBook-Pro.local>
2022-08-11 17:33:48 -0500

    started js

--- README.md
+++ README.md
@@ -30,13 +30,10 @@ Users should be able to:
 - CSS custom properties
 - Flexbox
 - CSS Grid
-- Mobile-first workflow
 
 ### What I learned
 
-Use this section to recap over some of your major learnings while working through this project. Writing these out and providing code samples of areas you want to highlight is a great way to reinforce your own knowledge.
 
-To see how you can add code snippets, see below:
 
 
 ## Author
---
+++ script.js
@@ -0,0 +1,26 @@
+let btn1 = document.getElementById('btn1');
+let btn2 = document.getElementById('btn2');
+let btn3 = document.getElementById('btn3');
+let btn4 = document.getElementById('btn4');
+let btn5 = document.getElementById('btn5');
+let btnSub = document.getElementById('sub-btn');
+
+
+
+btn1.addEventListener('click', (event) =>{
+     event.preventDefault()});
+
+btn2.addEventListener('click', (event) =>{
+    event.preventDefault()});
+
+btn3.addEventListener('click', (event) =>{
+    event.preventDefault()});
+
+btn4.addEventListener('click', (event) =>{
+    event.preventDefault()});
+
+btn5.addEventListener('click', (event) =>{
+    event.preventDefault()});
+
+btnSub.addEventListener('click', (event) =>{
+    event.preventDefault()});
